c738c5e7125856a3f08db64673077a1e5a734a76
Ray Salvo <salvo.raymond@gmail.com>
2025-01-03 15:07:27 -0500

    EventEmitters, Output, and handling the events in the template.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,26 +1,22 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
-  avatar = input.required<string>();
-  name = input.required<string>();
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
-  // more efficient way to write the imagePath getter - as a signal
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  });
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
-
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
