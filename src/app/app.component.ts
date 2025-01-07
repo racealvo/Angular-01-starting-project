8360c0fa49a9d04698931e94543310d295c0628f
Ray Salvo <salvo.raymond@gmail.com>
2025-01-07 17:07:03 -0500

    Added task component which displays selected user name.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from "./task/task.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  name:string = 'Angular';
 
   onSelectUser(id: string) {
     console.log('User selected:', id);
+
+    this.name = DUMMY_USERS.find(user => user.id === id)!.name;
   }
 }
