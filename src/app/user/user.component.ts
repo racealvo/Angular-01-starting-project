644f94928cd2af1ecb8576a5f371140e112e37b3
Ray Salvo <salvo.raymond@gmail.com>
2025-01-23 12:58:55 -0500

    Added date pipe.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, input, Input, Output, output } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from "../tasks/shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
 })
 export class UserComponent {
   @Input({required: true}) user!: User;
